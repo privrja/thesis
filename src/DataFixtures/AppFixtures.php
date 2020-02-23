f4e373396b63184fac5a300ab53775b2ffad1198
privrja <privrja@gmail.com>
2020-02-23 11:46:13 +0100

    Installing dependencies

---
+++ src/DataFixtures/AppFixtures.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\User;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+
+class AppFixtures extends Fixture
+{
+    private $passwordEncoder;
+
+    public function __construct(UserPasswordEncoderInterface $passwordEncoder)
+    {
+        $this->passwordEncoder = $passwordEncoder;
+    }
+
+    public function load(ObjectManager $manager)
+    {
+        $user = new User();
+        $user->setNick("kokos");
+        $user->setMail("kokos@palma.cz");
+        $user->setRoles(["ROLE_USER"]);
+        $user->setPassword($this->passwordEncoder->encodePassword($user, 'the_new_password'));
+        $user->setApiToken("12345");
+
+        $manager->persist($user);
+
+        $user = new User();
+        $user->setNick("privrja");
+        $user->setMail("privrja@gmail.com");
+        $user->setRoles(["ROLE_USER"]);
+        $user->setPassword($this->passwordEncoder->encodePassword($user, 'nic'));
+        $manager->persist($user);
+
+        $user = new User();
+        $user->setNick("admin");
+        $user->setMail("admin");
+        $user->setRoles(["ROLE_ADMIN"]);
+        $user->setPassword($this->passwordEncoder->encodePassword($user, 'kokos'));
+        $manager->persist($user);
+
+        $manager->flush();
+    }
+}

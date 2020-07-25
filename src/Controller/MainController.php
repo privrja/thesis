7df3e7bf9d7e36a8d236ec150d835b6bc3c9ece1
privrja <privrja@gmail.com>
2020-07-25 19:24:45 +0200

    Documentation
    | docu auth

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use App\Base\ResponseHelper;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
+use Swagger\Annotations as SWG;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Response;
@@ -12,8 +13,20 @@ use Symfony\Component\Routing\Annotation\Route;
 class MainController extends AbstractController
 {
     /**
+     * Auth
      * @Route("/rest", name="rest", methods={"GET"})
      * @IsGranted("ROLE_USER")
+     *
+     * @SWG\Get(
+     *     tags={"Auth"},
+     *     security={
+     *         {"ApiKeyAuth":{}}
+     *     },
+     *     description="For login use header X-AUTH-TOKEN for first time with value: 'username:password', as a response you get API token. After that you send as a value this generated token.",
+     *     @SWG\Response(response="200", description="Return when user is logged in."),
+     *     @SWG\Response(response="401", description="Return when user is not logged in."),
+     * )
+     *
      */
     public function rest()
     {

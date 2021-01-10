57251dbf24252fe476f2f0c478a9cf4e737bcf6e
privrja <privrja@gmail.com>
2021-01-10 13:04:08 +0100

    Remove exclamation mark on end of errors

--- src/Constant/ErrorConstants.php
+++ src/Constant/ErrorConstants.php
@@ -4,11 +4,11 @@ namespace App\Constant;
 
 class ErrorConstants {
 
-    const ERROR_JSON_FORMAT = 'Wrong JSON format!';
-    const ERROR_VISIBILITY_FORMAT = 'Visibility has not supported format! Supported format is PUBLIC|PRIVATE!';
-    const ERROR_CONTAINER_NAME_EXISTS = 'Container with this name exists for same user!';
-    const ERROR_EMPTY_PARAMS = 'At least one parameter should not be empty!';
-    const ERROR_EMPTY_ID = 'You need to specify id!';
-    const ERROR_CONTAINER_NOT_EXISTS_FOR_USER = 'Container with specified id for user not exists!';
+    const ERROR_JSON_FORMAT = 'Wrong JSON format';
+    const ERROR_VISIBILITY_FORMAT = 'Visibility has not supported format! Supported format is PUBLIC|PRIVATE';
+    const ERROR_CONTAINER_NAME_EXISTS = 'Container with this name exists for same user';
+    const ERROR_EMPTY_PARAMS = 'At least one parameter should not be empty';
+    const ERROR_EMPTY_ID = 'You need to specify id';
+    const ERROR_CONTAINER_NOT_EXISTS_FOR_USER = 'Container with specified id for user not exists';
 
 }

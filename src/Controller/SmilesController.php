36914b17e8fa12d1dc157c4cc0126d0dfe30e302
privrja <privrja@gmail.com>
2021-01-22 14:21:11 +0100

    Smiles
    | refactoring

--- src/Controller/SmilesController.php
+++ src/Controller/SmilesController.php
@@ -30,6 +30,15 @@ class SmilesController extends AbstractController {
      *
      * @SWG\Get(
      *     tags={"SMILES"},
+     *     @SWG\Parameter(
+     *          name="body",
+     *          in="body",
+     *          type="string",
+     *          required=true,
+     *          description="One param: smiles. Shouldn't be empty. SMILES is special format of molecular structures like: CCC(C)C1C(=O)NC(C(=O)NCCCC(C(=O)NC(C(=O)N2CCCC2C(=O)N1)CC3=CC=CC=C3)NC(=O)C(C(C)CC)NC(=O)C)C(C)CC",
+     *          @SWG\Schema(type="string",
+     *              example="{""smiles"":""OC(C(C(CC)C)N)=O""}"),
+     *      ),
      *     @SWG\Response(response="200", description="Return Unique SMILES."),
      *     @SWG\Response(response="401", description="Return when input is wrong."),
      *)

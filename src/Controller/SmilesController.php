1d285c8756962c08cd7ba9d9950b6081a3ae05d2
privrja <privrja@gmail.com>
2021-02-26 08:58:52 +0100

    Recomputing formula and mass

--- src/Controller/SmilesController.php
+++ src/Controller/SmilesController.php
@@ -2,7 +2,10 @@
 
 namespace App\Controller;
 
+use App\Base\FormulaHelper;
+use App\Exception\IllegalStateException;
 use App\Smiles\SmilesHelper;
+use App\Structure\FormulaMass;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
@@ -47,6 +50,52 @@ class SmilesController extends AbstractController {
         }
         return new JsonResponse(SmilesHelper::unique($smilesInput, $length));
     }
+
+    /**
+     * @Route("/rest/smiles/formula", name="smiles_formula", methods={"POST"})
+     * @param Request $request
+     * @return JsonResponse
+     *
+     * @SWG\Post(
+     *     tags={"SMILES"},
+     *     @SWG\Parameter(
+     *          name="body",
+     *          in="body",
+     *          type="string",
+     *          required=true,
+     *          description="Array of smiles objects: Shouldn't be empty. SMILES is special format of molecular structures like: CCC(C)C1C(=O)NC(C(=O)NCCCC(C(=O)NC(C(=O)N2CCCC2C(=O)N1)CC3=CC=CC=C3)NC(=O)C(C(C)CC)NC(=O)C)C(C)CC",
+     *          @SWG\Schema(type="string",
+     *              example="[{""smiles"": ""CC(=O)CC""}]"),
+     *      ),
+     *     @SWG\Response(response="200", description="Return Unique SMILES."),
+     *     @SWG\Response(response="400", description="Return when input is wrong."),
+     *)
+     */
+    public function computeFormulaMass(Request $request) {
+        $smilesInput = SmilesHelper::checkInputJson($request);
+        if ($smilesInput instanceof JsonResponse) {
+            return $smilesInput;
+        }
+        $length = count($smilesInput);
+        $nextCheck = SmilesHelper::checkNext($smilesInput, $length);
+        if ($nextCheck instanceof JsonResponse) {
+            return $nextCheck;
+        }
+        $res = [];
+        foreach ($smilesInput as $smiles) {
+            $resObject = new FormulaMass();
+            $resObject->smiles = $smiles->smiles;
+            $resObject->formula = FormulaHelper::formulaFromSmiles($smiles->smiles);
+            try {
+                $resObject->mass = FormulaHelper::computeMass($resObject->formula);
+            } catch (IllegalStateException $e) {
+                /** Empty on purpose */
+            }
+            array_push($res, $resObject);
+        }
+        return new JsonResponse($res);
+    }
+
 
 
 }
